750f938f3e5d1698ee5fa5da7ddecf93dd98c08d
iksflow <theiksflow@gmail.com>
2021-02-23 23:05:13 +0900

    2021.02.23 스프링 시큐리티 테스트코드에 적용하기

---
+++ src/main/java/com/iksflow/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.iksflow.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
